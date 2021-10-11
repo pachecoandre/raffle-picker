c84b6c225028579e86d074d6a57dbb023af792f3
pacheco <andrephenrique@hotmail.com>
2021-10-10 22:49:07 -0300

    code clean

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,16 @@
 import { config } from "dotenv";
-import Server from "./server/bot.server";
+import Server from "./server";
 import DB from './db'
 
+// environment variables import
 const env = process.env.NODE_ENV || "development";
 config({ path: `./config/${env}.env` });
 console.log(`Env mode: ${env}.`);
 
-
+// database connection
 DB.connect()
 
+// start server
 const server = new Server(process.env.PORT);
 server.route();
 server.listen();
