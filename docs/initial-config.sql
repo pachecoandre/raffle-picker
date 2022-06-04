4521871840fab7975e7ab5c12c811c12b90a6470
pacheco <andrephenrique@hotmail.com>
2022-06-04 14:59:18 -0300

    docs

---
+++ docs/initial-config.sql
@@ -0,0 +1,71 @@
+CREATE TABLE `users` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255),
+  `email` varchar(255)
+);
+
+CREATE TABLE `user_relationship` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int,
+  `campaign_id` int,
+  `role` varchar(255)
+);
+
+CREATE TABLE `campaigns` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255),
+  `estimated_draw_date` datetime,
+  `draw_date` datetime,
+  `raffle_price` float,
+  `created_at` timestamp
+);
+
+CREATE TABLE `prizes` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255),
+  `description` varchar(255),
+  `imageUrl` varchar(255),
+  `campaign_id` int
+);
+
+CREATE TABLE `prize_items` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `prize_id` int
+);
+
+CREATE TABLE `raffles` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `seller_id` int,
+  `participant_id` int,
+  `date` datetime,
+  `campaign_id` int,
+  `created_at` timestamp
+);
+
+CREATE TABLE `drawings` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `prize_item_id` int,
+  `raffle_id` int,
+  `campaign_id` int,
+  `created_at` timestamp
+);
+
+ALTER TABLE `user_relationship` ADD FOREIGN KEY (`campaign_id`) REFERENCES `campaigns` (`id`);
+
+ALTER TABLE `user_relationship` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `prizes` ADD FOREIGN KEY (`campaign_id`) REFERENCES `campaigns` (`id`);
+
+ALTER TABLE `prize_items` ADD FOREIGN KEY (`prize_id`) REFERENCES `prizes` (`id`);
+
+ALTER TABLE `raffles` ADD FOREIGN KEY (`seller_id`) REFERENCES `user_relationship` (`id`);
+
+ALTER TABLE `raffles` ADD FOREIGN KEY (`participant_id`) REFERENCES `user_relationship` (`id`);
+
+ALTER TABLE `raffles` ADD FOREIGN KEY (`campaign_id`) REFERENCES `campaigns` (`id`);
+
+ALTER TABLE `drawings` ADD FOREIGN KEY (`prize_item_id`) REFERENCES `prize_items` (`id`);
+
+ALTER TABLE `drawings` ADD FOREIGN KEY (`raffle_id`) REFERENCES `raffles` (`id`);
+
+ALTER TABLE `drawings` ADD FOREIGN KEY (`campaign_id`) REFERENCES `campaigns` (`id`);
