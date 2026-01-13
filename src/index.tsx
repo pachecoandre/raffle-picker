3fbbafb5ad7563bef587da0692fe3d7f5e326716
pacheco <andrephenrique@hotmail.com>
2026-01-13 12:19:27 -0300

    prettier format

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './components/App.tsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './components/App.tsx';
 
-const rootNode = document.getElementById('root')
-const root = createRoot(rootNode as HTMLElement)
-root.render(<App />)
+const rootNode = document.getElementById('root');
+const root = createRoot(rootNode as HTMLElement);
+root.render(<App />);
