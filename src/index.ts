48cef88a9fb4826feff810eb3c6a6aa7fbde9109
pacheco <andrephenrique@hotmail.com>
2026-01-27 15:24:37 -0300

    catch block on db connection

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import "dotenv/config";
 import Server from "./server";
 import db from "./db";
 
-db.getConnection().then(() => console.log("connected to database"));
+db.getConnection()
+  .then(() => console.log("connected to database"))
+  .catch((error) => console.error(error));
 
 const server = new Server();
 server.route();
