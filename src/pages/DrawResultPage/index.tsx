f6612dbeca7a719ebb7143b2ea5ea5c7d503f1bb
pacheco <andrephenrique@hotmail.com>
2026-04-26 16:46:29 -0300

    draw result page i18n

--- src/pages/DrawResultPage/index.tsx
+++ src/pages/DrawResultPage/index.tsx
@@ -1,23 +1,20 @@
 import React, { FC, useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import Container from '../../components/Container';
 import Section from '../../components/Section';
 import Title from '../../components/Title';
 import useDrawItems from '../../hooks/useDrawItems';
 import MainLayout from '../../components/MainLayout';
-import { Breadcrumb, Table } from 'antd';
+import { Breadcrumb, Button, Table } from 'antd';
 import { getCampaign } from '../../client';
 import { ICampaign } from '../Campaign/types';
 import { DrawResult } from '../../client/types';
 
-const columns = [
-  { title: 'Prize', dataIndex: 'prizeName', key: 'prizeName' },
-  { title: 'Name', dataIndex: 'winnerName', key: 'winnerName' },
-  { title: 'Phone', dataIndex: 'winnerPhone', key: 'winnerPhone' }
-];
-
 const Prizes: FC = () => {
   const { campaignId } = useParams();
+  const navigate = useNavigate();
+  const { t } = useTranslation();
   const drawItems = useDrawItems();
 
   const [campaign, setCampaign] = useState<ICampaign>({});
@@ -32,18 +29,29 @@ const Prizes: FC = () => {
       });
   }, [campaignId]);
 
+  const columns = [
+    { title: t('drawResult.prize'), dataIndex: 'prizeName', key: 'prizeName' },
+    { title: t('common.name'), dataIndex: 'winnerName', key: 'winnerName' },
+    { title: t('newRaffleTicket.phone'), dataIndex: 'winnerPhone', key: 'winnerPhone' }
+  ];
+
   return (
     <MainLayout>
       <Breadcrumb
         items={[
-          { title: <Link to="/">Campaigns</Link> },
+          { title: <Link to="/">{t('campaigns.title')}</Link> },
           { title: <Link to={`/campaigns/${campaignId}`}>{campaign?.name}</Link> },
-          { title: 'Prizes' }
+          { title: t('prizes.title') }
         ]}
       />
       <Container>
         <Section>
-          <Title backLink={`/campaigns/${campaignId}`}>Raffle Draw of {campaign?.name}</Title>
+          <Title backLink={`/campaigns/${campaignId}`}>
+            {t('drawResult.pageTitle', { name: campaign?.name })}
+          </Title>
+        </Section>
+        <Section>
+          <Button onClick={() => navigate(`/campaigns/${campaignId}`)}>{t('common.back')}</Button>
         </Section>
         <Section>
           <Table<DrawResult>
